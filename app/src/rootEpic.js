af459745fdd08039fdc3c89e02390be2b8450e13
Mateo Quintero <mquintero@H8269.ad.hugeinc.com>
2017-07-17 12:02:51 -0500

    Removed all epic stuff

--- app/src/rootEpic.js
+++
@@ -1,8 +0,0 @@
-import { combineEpics } from 'redux-observable';
-
-import login from './loginEpic';
-import messages from './messagesEpic';
-
-const rootEpic = combineEpics(login, messages);
-
-export default rootEpic;
